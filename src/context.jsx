ad8b5b8039fbd57c81072903885ed2c1aee201ad
Suraj Patil <suraj.gd89@gmail.com>
2023-05-05 19:47:22 +0530

    newest

--- src/context.jsx
+++ src/context.jsx
@@ -2,14 +2,14 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 const AppContext = createContext();
 const AppProvider = ({ children }) => {
-    const userID = new URLSearchParams(window.location.search).get('userID');
+    const userId = new URLSearchParams(window.location.search).get('userId');
     const [userData, setUserData] = useState();
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState();
     const getUserData = async () => {
         try {
             const response = await axios.get(
-                `http://localhost:3000/users?userID=${userID}`
+                `http://localhost:3000/users?userId=${userId}`
             );
             setTimeout(() => {
                 setLoading(false);
